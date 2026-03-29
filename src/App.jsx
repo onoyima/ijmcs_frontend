bebaad86fa2d3222756f84c300d2dc37594f7583
onoyima <clintonfaze@outlook.com>
2026-03-29 19:16:22 +0100

    history

--- src/App.jsx
+++ src/App.jsx
@@ -40,17 +40,7 @@ function AppContent() {
       }} />
       <Navbar />
       <main className="flex-grow relative">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
-          >
-            <AppRouter />
-          </motion.div>
-        </AnimatePresence>
+        <AppRouter />
       </main>
       <Footer />
     </div>
